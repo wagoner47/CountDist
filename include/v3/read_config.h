953e7eca42fc85eff496c16938475e234d8a6ab4
wagoner47 <wagoner47@email.arizona.edu>
2018-03-26 14:21:58 -0700

    Fixed some bugs with input and output operators, and added cmake commands to setup.py

--- include/v3/read_config.h
+++ include/v3/read_config.h
@@ -22,38 +22,9 @@ namespace configuration {
         }
 	};
 
-	std::istream& operator >> (std::istream& ins, data& d) {
-	    std::string s, key, value;
+	std::istream& operator>> (std::istream& ins, data& d);
 
-	    while (std::getline(ins, s)) {
-	        std::string::size_type begin = s.find_first_not_of(" \f\t\v");
-	        if (begin == std::string::npos) continue;
-
-	        if (std::string("#").find(s[begin]) != std::string::npos) continue;
-
-	        std::string::size_type end = s.find('=', begin);
-	        key = s.substr(begin, end - begin);
-
-	        key.erase(key.find_last_not_of(" \f\t\v") + 1);
-
-	        if (key.empty()) continue;
-
-	        begin = s.find_first_not_of(" \f\n\r\t\v", end + 1);
-	        end = s.find_last_not_of(" \f\n\r\t\v") + 1;
-
-	        value = s.substr(begin, end - begin);
-
-	        d[key] = value;
-	    }
-	    return ins;
-	}
-
-	std::ostream& operator << (std::ostream& outs, const data& d) {
-	    for(const auto iter : data) {
-	        outs << iter->first << " = " << iter->last << std::endl;
-	    }
-	    return outs;
-	}
+	std::ostream& operator<< (std::ostream& outs, const data& d);
 }
 
 configuration::data reader(const std::string& fname);
