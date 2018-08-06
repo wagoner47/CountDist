dc81116a4da80587af947efd498412fe653476ce
wagoner47 <wagoner47@email.arizona.edu>
2018-08-06 16:53:56 -0700

    Update __init__.py

--- countdist2/__init__.py
+++ countdist2/__init__.py
@@ -6,7 +6,7 @@ version = __version__
 cd_dir = os.path.dirname(__file__)
 
 #from .calc_distances import run_calc
-from .file_io import make_query, read_db, read_db_multiple
+from .file_io import make_query, read_db, read_db_multiple, read_metadata
 from .utils import MyConfigObj
 from . import calc_distances
 from .calc_distances import run_calc
