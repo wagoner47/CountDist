5ceed2378618ae0a3278d2ad324d9090bf73d1f1
wagoner47 <wagoner47@email.arizona.edu>
2018-09-17 09:30:31 -0700

    Remove unneccessary python functions for SO library setup

--- countdist2/__init__.py
+++ countdist2/__init__.py
@@ -7,7 +7,6 @@ cd_dir = os.path.dirname(__file__)
 
 #from .calc_distances import run_calc
 import calculate_distances as _calculate_distances
-from .file_io import make_query, read_db, read_db_multiple, read_metadata
 from .fit_probabilities import SingleFitter, AnalyticSingleFitter, \
     ProbFitter, TooFewGroupsError
 from .utils import MyConfigObj
