c21a86f696007ea10a62a220140b61e0e4d16138
Erika Wagoner <wagoner47@email.arizona.edu>
2019-01-17 11:43:13 -0700

    Add BinSpecifier to __init__ for direct user access

--- countdist2/__init__.py
+++ countdist2/__init__.py
@@ -7,6 +7,7 @@ cd_dir = os.path.dirname(__file__)
 
 #from .calc_distances import run_calc
 import calculate_distances as _calculate_distances
+from calculate_distances import BinSpecifier
 from .options import set_logging_level
 from .fit_probabilities import (SingleFitter, AnalyticSingleFitter,
                                 ProbFitter, TooFewGroupsError,
