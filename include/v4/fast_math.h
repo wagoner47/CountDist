1adb033e69821e48ac25a5881a7ef4f173159ac1
wagoner47 <wagoner47@email.arizona.edu>
2019-04-08 13:15:04 -0700

    Use lambda function in transform call of fill_vector_from_func

--- include/v4/fast_math.h
+++ include/v4/fast_math.h
@@ -31,101 +31,112 @@ namespace arrays {
         template<class>
         struct is_ref_wrapper : std::false_type {};
         template<class T>
-        struct is_ref_wrapper<std::reference_wrapper<T>> : std::true_type {};
+        struct is_ref_wrapper<std::reference_wrapper < T>> : std::true_type {
+    };
 
-        template<class T>
-        using not_ref_wrapper = std::negation<is_ref_wrapper<std::decay_t<T>>>;
+    template<class T>
+    using not_ref_wrapper = std::negation<is_ref_wrapper<std::decay_t < T>>>;
 
-        template<class D, class...>
-        struct return_type_helper { using type = D; };
-        template<class... Ts>
-        struct return_type_helper<void, Ts...> : std::common_type<Ts...> {
-          static_assert(std::conjunction_v<not_ref_wrapper<Ts>...>,
-                        "Types cannot contain reference_wrappers when D is void");
-        };
+    template<class D, class...>
+    struct return_type_helper { using type = D; };
+    template<class... Ts>
+    struct return_type_helper<void, Ts...> : std::common_type<Ts...> {
+      static_assert(std::conjunction_v < not_ref_wrapper<Ts>...>,
+      "Types cannot contain reference_wrappers when D is void");
+    };
 
-        template<class D, class... Ts>
-        using return_type = std::array<
-                typename return_type_helper<
-                        D,
-                        Ts...>::type,
-                sizeof...(Ts)>;
+    template<class D, class... Ts>
+    using return_type = std::array<
+            typename return_type_helper<
+                    D,
+                    Ts...>::type,
+            sizeof...(Ts)>;
 
-        template<typename T, typename dcy = std::decay_t<T>, std::size_t... Is>
-        std::array<dcy, sizeof...(Is)>
-        make_filled_array_helper(T&& t, std::index_sequence<Is...>) {
-            return {{(static_cast<void>(Is), t)...}};
+    template<typename T, typename dcy = std::decay_t<T>, std::size_t... Is>
+    std::array<dcy, sizeof...(Is)>
+    make_filled_array_helper(T&& t, std::index_sequence<Is...>) {
+        return {{(static_cast<void>(Is), t)...}};
+    }
+}
+
+template<class D = void, class... Ts>
+constexpr details::return_type<D, Ts...> make_array(Ts&& ... t) {
+    return {std::forward<Ts>(t)...};
+}
+
+template<std::size_t N, typename T, typename dcy = std::decay_t<T>>
+std::array<dcy, N> make_filled_array(T&& t) {
+    return details::make_filled_array_helper(t,
+                                             std::make_index_sequence < N > {});
+}
+
+template<typename T, std::size_t N, typename std::enable_if_t<
+        std::is_arithmetic_v <
+        T>, int> = 0>
+
+std::array<T, N> make_filled_array() {
+    return make_filled_array<N>((T) 0);
+}
+
+template<typename T, std::size_t N, typename std::enable_if_t<
+        !std::is_arithmetic_v <
+        T>, int> = 0>
+
+std::array<T, N> make_filled_array() {
+    return make_filled_array<N>(T());
+}
+
+template<typename T,
+         typename U,
+         typename R = decltype(std::declval
+                               < std::decay_t
+                               < T >> ()
+                                      * std::declval
+                               < std::decay_t
+                               < U >> ()),
+        std::size_t N>
+std::array<R, N>
+multiply_array_by_constant(const std::array<T, N>& arr, U&& x) {
+    std::array<R, N> ret = make_filled_array<R, N>();
+    std::transform(arr.begin(),
+                   arr.end(),
+                   ret.begin(),
+                   [=](T el) { return x * el; });
+    return ret;
+}
+
+template<std::size_t R, typename T, std::size_t N>
+std::array<T, R * N> repeat_array(const std::array<T, N>& arr) {
+    std::array < T, R * N > ret = make_filled_array<T, R * N>();
+    for (std::size_t i = 0; i < N; i++) {
+        for (std::size_t j = 0; j < R; j++) {
+            ret[i + (j * N)] = arr[i];
         }
     }
+    return ret;
+}
 
-    template<class D = void, class... Ts>
-    constexpr details::return_type<D, Ts...> make_array(Ts&& ... t) {
-        return {std::forward<Ts>(t)...};
-    }
-
-    template<std::size_t N, typename T, typename dcy = std::decay_t<T>>
-    std::array<dcy, N> make_filled_array(T&& t) {
-        return details::make_filled_array_helper(t,
-                                                 std::make_index_sequence<N>{});
-    }
-
-    template<typename T, std::size_t N, typename std::enable_if_t<
-            std::is_arithmetic_v<
-                    T>, int> = 0>
-    std::array<T, N> make_filled_array() {
-        return make_filled_array<N>((T) 0);
-    }
-
-    template<typename T, std::size_t N, typename std::enable_if_t<
-            !std::is_arithmetic_v<
-                    T>, int> = 0>
-    std::array<T, N> make_filled_array() {
-        return make_filled_array<N>(T());
-    }
-
-    template<typename T,
-             typename U,
-             typename R = decltype(std::declval<std::decay_t<T>>()
-                                   * std::declval<std::decay_t<U>>()),
-            std::size_t N>
-    std::array<R, N>
-    multiply_array_by_constant(const std::array<T, N>& arr, U&& x) {
-        std::array<R, N> ret = make_filled_array<R, N>();
-        std::transform(arr.begin(),
-                       arr.end(),
-                       ret.begin(),
-                       [=](T el) { return x * el; });
-        return ret;
-    }
-
-    template<std::size_t R, typename T, std::size_t N>
-    std::array<T, R * N> repeat_array(const std::array<T, N>& arr) {
-        std::array<T, R * N> ret = make_filled_array<T, R * N>();
-        for (std::size_t i = 0; i < N; i++) {
-            for (std::size_t j = 0; j < R; j++) {
-                ret[i + (j * N)] = arr[i];
-            }
-        }
-        return ret;
-    }
 }
 
 namespace math {
     using namespace std::string_view_literals;
 
     template<typename T, typename std::enable_if_t<
-            std::is_unsigned_v<T>,
+            std::is_unsigned_v < T>,
             int> = 0>
+
     constexpr int signof(T x) { return T(0) < x; }
 
     template<typename T, typename std::enable_if_t<
-            std::is_signed_v<T>,
+            std::is_signed_v < T>,
             int> = 0>
+
     constexpr int signof(T x) { return (T(0) < x) - (x < T(0)); }
 
     template<typename T, typename U, typename std::enable_if_t<
-            std::is_floating_point_v<T> || std::is_floating_point_v<U>,
-            int> = 0>
+            std::is_floating_point_v < T> || std::is_floating_point_v<U>,
+    int> = 0>
+
     constexpr bool isclose(T a, U b, int ulp = 10) {
         // isclose for non-integer numbers
         using R = decltype(std::declval<T>() - std::declval<U>());
@@ -135,14 +146,16 @@ namespace math {
     }
 
     template<typename T, typename U, typename std::enable_if_t<
-            std::is_integral_v<T> && std::is_integral_v<U>, int> = 0>
+            std::is_integral_v < T>&& std::is_integral_v<U>, int> = 0>
+
     constexpr bool isclose(T a, U b, int= 10) {
         // isclose for integers: exact equality
         return signof(a) * a == signof(b) * b;
     }
 
     template<typename T, typename U, typename std::enable_if_t<
-            std::is_arithmetic_v<T> && std::is_arithmetic_v<U>, int> = 0>
+            std::is_arithmetic_v < T>&& std::is_arithmetic_v<U>, int> = 0>
+
     inline bool isclose(std::vector<T> a, std::vector<U> b, int ulp = 10) {
         if (a.size() != b.size()) { return false; }
         return std::equal(a.begin(),
@@ -161,9 +174,10 @@ namespace math {
 
     template<std::size_t N, std::size_t M, typename T,
                                            typename U, typename std::enable_if_t<
-                    N == M && std::is_arithmetic_v<T>
-                    && std::is_arithmetic_v<U>,
-                    int> = 0>
+                    N == M && std::is_arithmetic_v < T>
+            && std::is_arithmetic_v<U>,
+            int> = 0>
+
     inline bool isclose(const std::array<T, N>& a,
                         const std::array<U, M>& b,
                         int ulp = 10) {
@@ -203,27 +217,31 @@ namespace math {
     constexpr static std::string_view snan = "NaN"sv;
 
     template<typename T, typename std::enable_if_t<
-            std::is_floating_point_v<T>,
+            std::is_floating_point_v < T>,
             int> = 0>
+
     constexpr bool isnan(T x) { return !isclose(x, x); }
 
     template<typename T, typename std::enable_if_t<
-            std::is_integral_v<T>,
+            std::is_integral_v < T>,
             int> = 0>
+
     constexpr bool isnan(T x) { return isclose(x, nan<T>); }
 
     template<typename T,
              typename dcy = std::decay_t<T>, typename std::enable_if_t<
-                    std::is_floating_point_v<
-                            T>, int> = 0>
+                    std::is_floating_point_v <
+                    T>, int> = 0>
+
     constexpr dcy inverse(T x) {
         return isclose(x, (T) 0) ? nan<dcy> : (dcy) (1.0 / x);
     }
 
     template<typename T,
              typename dcy = std::decay_t<T>, typename std::enable_if_t<
-                    std::is_integral_v<
-                            T>, int> = 0>
+                    std::is_integral_v <
+                    T>, int> = 0>
+
     constexpr dcy inverse(T x) {
         return isclose(x, (T) 0) ? (dcy) 0 : (dcy) (1 / x);
     }
@@ -236,8 +254,9 @@ namespace math {
 
     template<typename T,
              typename dcy = std::decay_t<T>, typename std::enable_if_t<
-                    std::is_arithmetic_v<
-                            T>, int> = 0>
+                    std::is_arithmetic_v <
+                    T>, int> = 0>
+
     constexpr dcy square(T x) { return x * x; }
 
     constexpr long double factorial(const std::intmax_t& n) {
@@ -259,14 +278,15 @@ namespace math {
 
     public:
         constexpr static array_type
-                coeffs = _coeffs(std::make_index_sequence<N>{});
+                coeffs = _coeffs(std::make_index_sequence < N > {});
     };
 
 
     template<class Base, std::size_t N,
              class dcy = std::decay_t<typename Base::value_type>, typename std::enable_if_t<
-                    std::is_floating_point_v<dcy>,
-                    int> = 0>
+                    std::is_floating_point_v < dcy>,
+            int> = 0>
+
     inline dcy _trig(typename Base::value_type x) noexcept {
         using c = trig_coeffs<Base, N>;
         if (std::isnan(x) && std::numeric_limits<dcy>::has_quiet_NaN) {
@@ -292,8 +312,10 @@ namespace math {
 
     namespace detail {
         template<typename T, typename std::enable_if_t<
-                std::is_arithmetic_v<T>,
+                std::is_arithmetic_v < T>,
                 int> = 0>
+
+
         struct _sin {
           using value_type = T;
 
@@ -318,8 +340,10 @@ namespace math {
 
 
         template<typename T, typename std::enable_if_t<
-                std::is_arithmetic_v<T>,
+                std::is_arithmetic_v < T>,
                 int> = 0>
+
+
         struct _cos {
           using value_type = T;
 
@@ -344,12 +368,12 @@ namespace math {
 
     template<class T, size_t N = detail::_sin<T>::default_N>
     constexpr static std::decay_t<T>
-    sin(T x) noexcept { return _trig<detail::_sin<T>, N>(x); }
+    sin(T x) noexcept { return _trig < detail::_sin<T>, N > (x); }
 
     template<class T, size_t N = detail::_cos<T>::default_N>
     constexpr static std::decay_t<T> cos(T x) noexcept {
-        return 1 - _trig<
-                detail::_cos<T>, N>(x);
+        return 1 - _trig <
+               detail::_cos<T>, N > (x);
     }
 
 }
@@ -365,7 +389,10 @@ namespace arrays {
         std::iota(indices.begin(), indices.end(), min_index);
         std::vector<T> vec;
         std::cout << "Transform indices" << std::endl;
-        std::transform(indices.begin(), indices.end(), vec.begin(), gen_func);
+        std::transform(indices.begin(),
+                       indices.end(),
+                       vec.begin(),
+                       [](int i) { return gen_func(i); });
         std::cout << "Returning" << std::endl;
         return vec;
     }
